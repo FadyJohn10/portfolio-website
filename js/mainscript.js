04d7cace42264a00f41016674452a8d1f947ea14
FadyJohn10 <63600279+FadyJohn10@users.noreply.github.com>
2021-05-24 14:38:19 +0200

    2

--- js/mainscript.js
+++ js/mainscript.js
@@ -6,11 +6,33 @@ $(function () {
     $('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
-		scrollHorizontally: true
+		scrollHorizontally: false
 	});
 
 	//methods
 	$.fn.fullpage.setAllowScrolling(false);
+
+  /* ===============================  Mouse effect  =============================== */
+
+function mousecursor() {
+    if ($("body")) {
+        const e = document.querySelector(".cursor-inner"),
+            t = document.querySelector(".cursor-outer");
+        let n, i = 0,
+            o = !1;
+        window.onmousemove = function (s) {
+            o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+        }, $("body").on("mouseenter", "a, .cursor-pointer", function () {
+            e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+        }), $("body").on("mouseleave", "a, .cursor-pointer", function () {
+            $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+        }), e.style.visibility = "visible", t.style.visibility = "visible"
+    }
+};
+
+$(function () {
+    mousecursor();
+});
 
 
     //parallax movement
@@ -149,5 +171,94 @@ $(function () {
       },
       "retina_detect": true
     });
+
+    (function() {
+
+    var slidersContainer = document.querySelector('.sliders-container');
+
+    // Initializing the numbers slider
+    var msNumbers = new MomentumSlider({
+        el: slidersContainer,
+        cssClass: 'ms--numbers',
+        range: [1, 4],
+        rangeContent: function (i) {
+            return '0' + i;
+        },
+        style: {
+            transform: [{scale: [0.4, 1]}],
+            opacity: [0, 1]
+        },
+        interactive: false
+    });
+
+    // Initializing the titles slider
+    var titles = [
+        'STEM October',
+        'Youth Science Journal',
+        'TEDx MaadiSTEM',
+        'Windy Sunset'
+    ];
+    var msTitles = new MomentumSlider({
+        el: slidersContainer,
+        cssClass: 'ms--titles',
+        range: [0, 3],
+        rangeContent: function (i) {
+            return '<h3>'+ titles[i] +'</h3>';
+        },
+        vertical: true,
+        reverse: true,
+        style: {
+            opacity: [0, 1]
+        },
+        interactive: false
+    });
+
+    // Initializing the links slider
+    var msLinks = new MomentumSlider({
+        vertical: true,
+        interactive: false
+    });
+
+    // Get pagination items
+    var pagination = document.querySelector('.pagination');
+    var paginationItems = [].slice.call(pagination.children);
+
+    // Initializing the images slider
+    var msImages = new MomentumSlider({
+        // Element to append the slider
+        el: slidersContainer,
+        // CSS class to reference the slider
+        cssClass: 'ms--images',
+        // Generate the 4 slides required
+        range: [0, 3],
+        rangeContent: function () {
+            return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+        },
+        // Syncronize the other sliders
+        sync: [msNumbers, msTitles, msLinks],
+        // Styles to interpolate as we move the slider
+        style: {
+            '.ms-slide__image': {
+                transform: [{scale: [1.5, 1]}]
+            }
+        },
+        // Update pagination if slider change
+        change: function(newIndex, oldIndex) {
+            if (typeof oldIndex !== 'undefined') {
+                paginationItems[oldIndex].classList.remove('pagination__item--active');
+            }
+            paginationItems[newIndex].classList.add('pagination__item--active');
+        }
+    });
+
+    // Select corresponding slider item when a pagination button is clicked
+    pagination.addEventListener('click', function(e) {
+        if (e.target.matches('.pagination__button')) {
+            var index = paginationItems.indexOf(e.target.parentNode);
+            msImages.select(index);
+        }
+    });
+
+})();
 
 });
